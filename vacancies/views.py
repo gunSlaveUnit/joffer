f54c59e8d310eb99a7cb5c5df86e74fedf9bb9bd
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-28 13:11:00 +0300

    Vacancy detail view

--- vacancies/views.py
+++ vacancies/views.py
@@ -6,3 +6,8 @@ from .models import Vacancy
 class VacancyList(generic.ListView):
     model = Vacancy
     context_object_name = "vacancy_list"
+
+
+class VacancyDetail(generic.DetailView):
+    model = Vacancy
+    context_object_name = "vacancy_detail"
