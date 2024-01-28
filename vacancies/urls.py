f9adee9427e63c957c45314bd01b2d17876891ae
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-01-28 13:11:06 +0300

    Vacancy detail url

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import VacancyList
+from .views import VacancyList, VacancyDetail
 
 urlpatterns = [
     path('', VacancyList.as_view(), name='vacancy_list'),
+    path('<int:pk>/', VacancyDetail.as_view(), name='vacancy_detail'),
 ]
