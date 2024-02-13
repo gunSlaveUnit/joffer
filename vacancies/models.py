f00ccd2229091018a0f47a5ed86b9364bc181124
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-13 22:13:39 +0300

    Experience from and to fields

--- vacancies/models.py
+++ vacancies/models.py
@@ -5,8 +5,12 @@ from root.models import Entity
 
 class Vacancy(Entity):
     title = models.CharField(max_length=100)
+
     salary_to = models.IntegerField(null=True, blank=True)
     salary_from = models.IntegerField(null=True, blank=True)
+
+    experience_to = models.IntegerField(null=True, blank=True)
+    experience_from = models.IntegerField(null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'vacancies'
