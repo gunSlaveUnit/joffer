4545c08b5094bbbb79944c1deb7517aca9fbd542
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-14 22:14:51 +0300

    Registered skill model

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Vacancy, Hiding
+from .models import Skill, Vacancy, Hiding
 
+admin.site.register(Skill)
 admin.site.register(Hiding)
 admin.site.register(Vacancy)
