5e3d68d0ab5db9acf4971e25022fae11327f1407
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-14 22:27:27 +0300

    Registered Favorite model

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Skill, Vacancy, Hiding
+from .models import Skill, Vacancy, Favorite, Hiding
 
 admin.site.register(Skill)
 admin.site.register(Hiding)
 admin.site.register(Vacancy)
+admin.site.register(Favorite)
