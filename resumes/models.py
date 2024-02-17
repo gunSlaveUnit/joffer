561f8dae901fd175b04e7506bef74ae73f1e625c
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-17 23:38:06 +0300

    Resume model

--- resumes/models.py
+++ resumes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from root.models import Entity
+
+
+class Resume(Entity):
+    position = models.CharField(max_length=100)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
