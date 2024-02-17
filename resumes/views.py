6a36d2a1256bbf91f27c4e257c9473a641260f9a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-18 00:04:54 +0300

    Resume detail view

--- resumes/views.py
+++ resumes/views.py
@@ -6,3 +6,8 @@ from .models import Resume
 class ResumeList(generic.ListView):
     model = Resume
     context_object_name = "resume_list"
+
+
+class ResumeDetail(generic.DetailView):
+    model = Resume
+    context_object_name = "resume_detail"
