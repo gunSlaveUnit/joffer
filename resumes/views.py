a382257892c97f6ae5c3545a3e9e3f6f01e4d1e1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-18 00:04:43 +0300

    Resume list view

--- resumes/views.py
+++ resumes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Resume
+
+
+class ResumeList(generic.ListView):
+    model = Resume
+    context_object_name = "resume_list"
