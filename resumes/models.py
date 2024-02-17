3b81c53bc600217276b6bc935007d9702121a94a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-17 23:48:16 +0300

    Trying to create a SkillLevel model. Every skill from resume may have from 1 to 5 points

--- resumes/models.py
+++ resumes/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
-from root.models import Entity
+from root.models import Entity, Skill
 
 
 class Resume(Entity):
     position = models.CharField(max_length=100)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+
+class SkillLevel(Entity):
+    LEVELS = (
+        (1, 'Beginner'),
+        (2, 'Intermediate'),
+        (3, 'Advanced'),
+        (4, 'Expert'),
+        (5, 'Master'),
+    )
+
+    level = models.IntegerField(choices=LEVELS)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
