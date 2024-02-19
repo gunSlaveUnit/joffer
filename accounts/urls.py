05ab6c034e55ce3c5c7c9d408097d7000a2440c9
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-19 22:57:05 +0300

    Profile url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from .views import RegisterView
+from .views import RegisterView, ProfileView
 
 urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('profile/', ProfileView.as_view(), name='profile'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
