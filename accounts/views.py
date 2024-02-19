71731e20d4907da8d0945fa378934fe83cd6ce6b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-19 22:56:59 +0300

    ProfileView

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,21 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, TemplateView
+
+from resumes.models import Resume
 
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/register.html'
+
+
+class ProfileView(TemplateView):
+    template_name = 'accounts/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        resumes = Resume.objects.filter(user=self.request.user)
+        context['resumes'] = resumes
+        return context
