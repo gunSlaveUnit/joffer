ebbd338122ca5f675a2fa371fb2c60a531edc2ad
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-22 10:47:15 +0300

    Registered WorkFormat

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Vacancy, Favorite, Hiding
+from .models import Vacancy, Favorite, Hiding, WorkFormat
 
 admin.site.register(Hiding)
 admin.site.register(Vacancy)
 admin.site.register(Favorite)
+admin.site.register(WorkFormat)
