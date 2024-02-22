0e91db61936a51cb66fb473837f78891d232a755
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-22 11:03:52 +0300

    EmploymentType registered

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Vacancy, Favorite, Hiding, WorkFormat
+from .models import Vacancy, Favorite, Hiding, WorkFormat, EmploymentType
 
 admin.site.register(Hiding)
 admin.site.register(Vacancy)
 admin.site.register(Favorite)
 admin.site.register(WorkFormat)
+admin.site.register(EmploymentType)
