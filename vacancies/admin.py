7ffcf7fd851b7977d6bf0bbe24302c635046908b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-24 23:58:07 +0300

    Registered respond model

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Vacancy, Favorite, Hiding, WorkFormat, EmploymentType
+from .models import Vacancy, Favorite, Hiding, WorkFormat, EmploymentType, Respond
 
 admin.site.register(Hiding)
 admin.site.register(Vacancy)
+admin.site.register(Respond)
 admin.site.register(Favorite)
 admin.site.register(WorkFormat)
 admin.site.register(EmploymentType)
